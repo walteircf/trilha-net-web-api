1236fef41ffea270c84622146aff26ea21234562
Walteir Coelho <wcfreitasf@gmail.com>
2025-08-14 21:41:05 -0300

    update: remove controllers sem uso do codigo

--- Controllers/ValuesController.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
-
-namespace ModuloAPI.Controllers {
-    [Route("[controller]")]
-    [ApiController]
-    public class ValuesController : ControllerBase {
-    }
-}
--- Controllers/ValuesController1.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
-
-namespace ModuloAPI.Controllers {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class ValuesController1 : ControllerBase {
-    }
-}
