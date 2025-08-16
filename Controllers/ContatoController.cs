4dbaea1247aa241be64b59f62657bfe149b05dda
Walteir <wcfreitasf@gmail.com>
2025-08-15 23:38:35 -0300

    feat: adiciona os endpoints do CRUD na API e adiciona metodo de busca por nome

--- Controllers/ContatoController.cs
+++ Controllers/ContatoController.cs
@@ -18,12 +18,63 @@ namespace ModuloAPI.Controllers
             _context = context;
         }
 
-        [HttpPost]
+        [HttpPost("CadastrarContato")]
         public IActionResult Create(Contato contato)
         {
             _context.Add(contato);
             _context.SaveChanges();
+            return CreatedAtAction(nameof(ObterPorId), new {id = contato.Id}, contato);
+        }
+
+        [HttpGet("BuscarPorId/{id}")]
+        public IActionResult ObterPorId(int id)
+        {
+            var contato = _context.Contatos.Find(id);
+
+            if (contato == null)
+                return NotFound();
+
             return Ok(contato);
+        }
+
+        [HttpGet("BuscarPorNome/{nome}")]
+        public IActionResult ObterPorNome(string nome)
+        {
+            var contatos = _context.Contatos.Where(x => x.Nome.Contains(nome));
+            if (contatos == null)
+                return NotFound();
+            return Ok(contatos);
+        }
+
+        [HttpPut("AtualizarContato/{id}")]
+        public IActionResult AtualizarContato(int id, Contato contato)
+        {
+            var contatoBanco = _context.Contatos.Find(id);
+
+            if (contatoBanco == null)
+                return NotFound();
+
+            contatoBanco.Nome = contato.Nome;
+            contatoBanco.Telefone = contato.Telefone;
+            contatoBanco.Ativo = contato.Ativo;
+
+            _context.Contatos.Update(contatoBanco);
+            _context.SaveChanges();
+
+            return Ok(contatoBanco);
+        }
+
+        [HttpDelete("Deletar/{id}")]
+        public IActionResult Deletar(int id)
+        {
+            var contatoBanco = _context.Contatos.Find(id);
+
+            if (contatoBanco == null)
+                return NotFound();
+
+            _context.Contatos.Remove(contatoBanco);
+            _context.SaveChanges();
+            return NoContent();
         }
     }
 }
